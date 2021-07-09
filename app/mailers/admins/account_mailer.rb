f45fe86d00352bce9d450b8b1292234b1c2634a4
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2021-07-09 10:59:25 +0100

    Send mail to ben account

--- app/mailers/admins/account_mailer.rb
+++ app/mailers/admins/account_mailer.rb
@@ -2,7 +2,7 @@ class Admins::AccountMailer < Mail::Notify::Mailer
   def account_creation_request(record, _opts = {})
     @template = NOTIFY_ADMIN_ACCOUNT_CREATION_REQUEST_TEMPLATE
     @account_request = record
-    @to = t("help.email")
+    @to = "ben.mitchell@digital.education.gov.uk"
 
     view_mail(@template, to: @to, subject: t(".subject"))
   end
